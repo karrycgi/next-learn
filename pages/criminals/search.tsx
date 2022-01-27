052a674654620cd9330b90ad9f1d4f75f9d8d592
AdrianAtCgi <97726753+AdrianAtCgi@users.noreply.github.com>
2022-01-27 08:05:30 +0100

    UserInput Component +Reusability für GET/POST

--- pages/criminals/search.tsx
+++ pages/criminals/search.tsx
@@ -1,15 +1,33 @@
-import { GetStaticProps, NextPage } from "next"
-import { RedNoticeDetails } from "next-learn-red-notice-api"
-import RedNotice, { searchRedNotice } from "next-learn-red-notice-api/build/lib/RedNotice.all"
-import { CriminalSidebarProps } from "../../components/CriminalsSidebar.component"
+import {  NextPage } from "next"
+import { RedNoticeQuery } from "next-learn-red-notice-api/build/lib/RedNotice.all"
 import UserInput from "../../components/input/UserInput.component"
 import LayoutComponent from "../../components/Layout.component"
 
 
 const CriminalSearchPage : NextPage = () => {
+    const onSub : (query: RedNoticeQuery) =>  void = async (query: RedNoticeQuery) => {
+        //TO DO:
+        //  do whatever request you want with query
+        console.log(query)
+    }
+    
+    const query :RedNoticeQuery =  {
+        forename: "string",
+        name: "string",
+        nationality: "string",
+        ageMax: 1,
+        ageMin: 2,
+        sexId: "F",
+        arrestWarrantCountryId: "string",
+        page: 1,
+        resultPerPage: 2,
+    }
+    
     return <div>
+
+        
         <LayoutComponent title="Search" description="Search Form for querying particular criminals" >
-                <UserInput/>
+                <UserInput value={query}  onSub={onSub}/>
         </LayoutComponent>        
     </div>
 }
