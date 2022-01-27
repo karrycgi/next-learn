6cabb3815e659168d3bd9e6e93a5b40af93c0a91
Martin Karry <martin.karry@cgi.com>
2022-01-27 14:59:22 +0100

    Demo formular

---
+++ pages/test.tsx
@@ -0,0 +1,10 @@
+import { NextPage } from "next";
+import SearchByName from "../components/dynamic/SearchByName.component";
+
+const TestPage: NextPage = () => {
+    return <div>
+        <SearchByName />
+    </div>
+}
+
+export default TestPage;
