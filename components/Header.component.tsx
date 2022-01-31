578845f9190a367bf6ff44bc6dad9a6a122ffac7
CSCGI <christian.steimel@cgi.com>
2022-01-31 15:40:41 +0100

    added navbar

--- components/Header.component.tsx
+++ components/Header.component.tsx
@@ -1,24 +1,22 @@
 import Link from "next/link";
 import LOGO from "../public/headerImage.png";
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import styles from "../styles/Header.module.css"
 
 const HeaderComponent = (): JSX.Element => {
     return <div>
-        <nav>
-            <ul>
-                <li> <Link href="/"><a>Home</a></Link> </li>
-                <li> <Link href="/criminals/search"><a>Search</a></Link> </li>
+            <ul className={styles.ul}>
+                <li> <Link href="/"><a className={styles.a}>Home</a></Link> </li>
+                <li> <Link href="/criminals/search"><a className={styles.a}>Search</a></Link> </li>
             </ul>
-        </nav>
 
         <p><Image alt="TODO" src={LOGO} width={1920}></Image>
 
             <h1>
-                Willkommen in unserer Verbrecherseite
+                Willkommen auf unserer Verbrecherseite
             </h1>
         </p>
-        <hr />
+        <hr/>
     </div>
 }
 
