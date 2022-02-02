59fca20adf3481e85cfe524ba43ff8243faaae96
CSCGI <christian.steimel@cgi.com>
2022-02-02 14:18:25 +0100

    moved navbar to seperate component

--- components/Header.component.tsx
+++ components/Header.component.tsx
@@ -1,24 +1,19 @@
 import Link from "next/link";
 import LOGO from "../public/headerImage.png";
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import NavbarComponent from "./Navbar.component";
 
 const HeaderComponent = (): JSX.Element => {
     return <div>
-        <nav>
-            <ul>
-                <li> <Link href="/"><a>Home</a></Link> </li>
-                <li> <Link href="/criminals/search"><a>Search</a></Link> </li>
-            </ul>
-        </nav>
+        <NavbarComponent></NavbarComponent>
 
         <p><Image alt="TODO" src={LOGO} width={1920}></Image>
 
             <h1>
-                Willkommen in unserer Verbrecherseite
+                Willkommen auf unserer Verbrecherseite
             </h1>
         </p>
-        <hr />
+        <hr/>
     </div>
 }
 
